ed621bef304dfd55191f13553929ecc336046ad4
omanchek <owen.manchek@mymail.champlain.edu>
2025-11-20 02:08:12 -0500

    Cell Data Navmesh Member Struct
    * creating a new member struct in navmesh class for tracking data on each tile being evaluated

--- assets/Navmesh.h
+++ assets/Navmesh.h
@@ -4,18 +4,26 @@
 #include "Cell.h"
 #include "NavData.h"
 #include "Obstacle.h"
+#include <queue>
 #include <raylib.h>
 #include <vector>
 
 class NavMesh
 {
+   struct cellData
+   {
+      Cell* parent = nullptr;
+      float g;
+      float h;
+   };
+
    public:
       NavMesh();
       NavMesh(Vector2 baseOffset, int inCellSize, int inHorizontal, int inVertical);
       ~NavMesh();
 
       void DrawNavmesh();
-
+      
       NavPath GetPathToPoint(Vector2 origin, Vector2 destination);
       NavPath GetPathToPoint(Cell origin, Cell destination);
       void RegisterObstacle(Obstacle* inObstacle);
